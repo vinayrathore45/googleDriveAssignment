32d1abad248177ca69c54f7a4b2f04a6fdf4fb41
Vinay Rathore <vinayrathore91190@gmail.com>
2024-02-10 19:55:18 +0530

    env2

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const progress = {
   download: 0,
   upload: 0
 };
-
+const serviceCredentials = JSON.parse(process.env.serviceCredentials);
 const auth = new google.auth.GoogleAuth({
-  keyFile: './serviceCredentials.json'/*JSON.parse(process.env.serviceCredentials)*/,
+  credentials: serviceCredentials,
   scopes: 'https://www.googleapis.com/auth/drive',
 });
 
